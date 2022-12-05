8c923ba96017111ca96e4c1b6a392b2841987b19
Anastasiia Shlapak <anastasiashlapak@icloud.com>
2022-12-05 13:43:17 +0100

    update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 class RangeValidator {
   /**
    *
@@ -6,49 +6,43 @@ class RangeValidator {
    * @param {number} to
    */
   constructor(from, to) {
-    this._from = from;
-    this._to = to;
-  }
-  get from() {
-    return this._from;
-  }
-  get to() {
-    return this._to;
+    this.from = from;
+    this.to = to;
   }
   set from(from) {
     if (typeof from !== "number") {
       throw new TypeError("from must be number");
     }
-    if (from < MIN_VALUE) {
+    else if (from > this._to) {
       throw new RangeError("from must be more than 0");
     }
     this._from = from;
   }
+  get from() {
+    return this._from;
+  }
   set to(to) {
-    if (this.from > to) {
-      throw new RangeError("to must be higher than from");
-    }
     if (typeof to !== "number") {
       throw new TypeError("to must be number");
     }
-    if (to > MAX_VALUE) {
-      throw new RangeError("to must be less than max value");
+    else if (to< this._from ) {
+      throw new RangeError("to must be higher than from");
     }
     this._to = to;
   }
+  get to() {
+    return this._to;
+  }
 
-  getRange() {
-    return `${this.from} to ${this.to} `;
+  get Range() {
+    return [this._from, this._to];
   }
   validate(number) {
     if (typeof number !== "number") {
       throw new TypeError("number must be number");
     }
-    if (number > 6 && number < 20) {
-      return true;
-    }
-    return false;
-  }
+    return number >= this._from && number <= this._to;
+}
 }
 
 try {
@@ -56,8 +50,9 @@ try {
   range.from = 2;
   range.to = 6;
   console.log(range);
-  console.log(range.getRange());
+  console.log(range.Range);
   console.log(range.validate(7));
 } catch (error) {
   console.log(error);
 }
+
